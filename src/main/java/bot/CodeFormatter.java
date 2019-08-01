0750117d8febcd41399cf6d1837679756c7cfcd3
KrossJL03 <jkross76@gmail.com>
2019-07-31 22:12:43 -0500

    Add code formatter interface

---
+++ src/main/java/bot/CodeFormatter.java
@@ -0,0 +1,13 @@
+package bot;
+
+import org.jetbrains.annotations.NotNull;
+
+public interface CodeFormatter
+{
+    /**
+     * Get style
+     *
+     * @return String
+     */
+    @NotNull String getStyle();
+}
