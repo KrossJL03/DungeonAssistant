9c180f1e5d3dd1f7c5553b85b5681144b98d4341
KrossJL03 <jkross76@gmail.com>
2019-08-02 20:06:26 -0500

    Update repository path

--- src/main/java/bot/Repository/RepositoryPaths.java
+++ src/main/java/bot/Repository/RepositoryPaths.java
@@ -1,7 +1,7 @@
 package bot.Repository;
 
 public class RepositoryPaths {
-    private static String DATABASE_CONNECTION = "jdbc:sqlite:src/main/java/bot/Repository/Database/";
+    private static String DATABASE_CONNECTION = "jdbc:sqlite:src/main/java/bot/Registry/Database/";
 
     public static String getDatabasePath(String dbName) {
         return String.format("%s/%s.db", RepositoryPaths.DATABASE_CONNECTION, dbName);
